54470d32560a4b5fcb7617547a82d078fec38746
Jorge Luis <jlhc1996@gmail.com>
2025-01-04 22:07:29 -0600

    final1

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -5,6 +5,20 @@ const Bienvenida = () => {
     <div>
       <section className="text-center py-10" data-aos="fade-up">
         <h1 className="font-alex-brush text-8xl text-gray-800">Sarahí & Iván</h1>
+        
+        <div className="flex justify-center my-8">
+          <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg">
+            <img className="w-full" src="/src/assets/Novios.jpg" alt="Novios" />
+          </div>
+        </div>
+       
+        <h2 className="font-parisienne text-4xl text-gray-800">
+          ¡Nos Casamos!
+        </h2>
+        <p className="font-cormorant mt-4 text-lg text-gray-600">
+          ¡Hola! Somos Sarahí e Iván, y estamos muy emocionados de celebrar
+          nuestra unión contigo.
+        </p>
         <p className="font-cormorant italic mt-4 text-lg text-gray-600">
           ¡Estamos encantados de invitarte a nuestra boda!
         </p>
--- src/components/Confirmacion.jsx
+++ src/components/Confirmacion.jsx
@@ -4,12 +4,12 @@ import React from "react";
 const Confirmacion = () => {
   return (
     <div>
-      <section className="py-10  text-center" data-aos="fade-up">
-        <h2 className="text-3xl font-semibold text-gray-800">RSVP</h2>
+      <section className="py-10 text-center" data-aos="fade-up">
+        <h2 className="text-3xl font-semibold text-gray-800">Confirmación de Asistencia</h2>
         <p className="mt-4 text-gray-600">
-          Por favor, confirma tu asistencia antes del 10 de enero de 2025.
+          Te pedimos que confirmes tu asistencia antes del 10 de enero de 2025 para poder organizar todo de la mejor manera posible.
         </p>
-        <button className="mt-6 px-8 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700">
+        <button className="mt-6 px-8 py-3 bg-blue-500 text-white font-semibold rounded-full shadow-lg hover:bg-blue-600">
           Confirmar Asistencia
         </button>
       </section>
